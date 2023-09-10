8ef9a5c7a3d6374aecfc6688e6e3359667dd499c
Nitish Kumar Tao <55820253+nktni30@users.noreply.github.com>
2023-09-10 10:59:33 +0530

    css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,19 +5,28 @@ import ToolsImg from '../img/banner/tools.png';
 const Banner = () => {
       return (
             <>
-            <div className='container-fluid'>
-                  <div className="banner row">
-                        <div className='col-sm-6 d-flex justify-content-center justify-content-lg-center align-items-lg-center align-items-end'>
-                              
-                              <div className='banner-text mt-lg-0 mt-sm-5'>
-                              <h1>Conterials.com</h1>
+            <div className='container-fluid banner'>
+                  <div className="container pt-5">
+                  <div className="row pt-5 pt-lg-5">
+                        <div className='col-sm-5 d-flex justify-content-center justify-content-lg-start align-items-lg-center align-items-end'>
+                              <div className="rwo">
+                              <div className='mt-lg-0 mt-sm-5'>
+                              <h1 className='text-center text-lg-start text-white'>Conterials.com</h1>
+                              <h6 className='text-center text-lg-start text-white'>
                                     We are<span> Open </span> Now
                                     Please Visit
+                                    
+                                    Offline Store at : Pragati Path, Samlong Ranchi-01
+                              </h6>
+                                    
                               </div>
+                              </div>
+                              
                         </div>
-                        <div className='col-sm-6 d-flex justify-content-center align-items-center'>
+                        <div className='col-sm-7 d-flex justify-content-center align-items-center'>
                               <img className='' src={ToolsImg} alt='banner1'/>
                         </div>
+                  </div>
                   </div>
                   </div>
             </>
