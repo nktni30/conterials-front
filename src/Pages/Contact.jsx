1629cd9ef6ceaeb0201649775dfc3cd95a0508df
Nitish Kumar Tao <55820253+nktni30@users.noreply.github.com>
2023-09-10 14:59:00 +0530

    update details

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BsGeoAltFill, BsTelephoneFill, BsEnvelopeFill } from 'react-icons/bs';
 import Footer from '../components/Footer';
 import BannerImg from '../img/about/banner-img.png';
-
+import { Link } from 'react-router-dom';
 
 
 
@@ -30,7 +30,7 @@ const Contact = () => {
           {/* map */}
 
           <div className='col-sm-12 col-lg-12'>
-            <iframe title='map' className='add-map mt-5 mb-4' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15135.177752045163!2d73.82902945000001!3d18.492968149999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bfe41da12903%3A0x19b1509c33c67f6a!2sKatakirr%20Misal!5e0!3m2!1sen!2sin!4v1688797502924!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe title='map' className='add-map mt-5 mb-4' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3662.9713506234775!2d85.34927387532379!3d23.353052478943404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e1e10932fa8d%3A0xbbfe698809a1cdab!2sConterials.com!5e0!3m2!1sen!2sin!4v1694337414691!5m2!1sen!2sin" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
 
           {/* contact */}
@@ -44,7 +44,7 @@ const Contact = () => {
                   </span>
                 </div>
                 <div className='col-8 col-md-8 col-lg-8 d-flex justify-content-start align-items-center'>
-                  <div className='contact-font text-lg-start'>Demo address , Ranchi, Jahrkhand -834001</div>
+                  <div className='contact-font text-lg-start'>Pragati Path, Samlong, Ranchi, Jharkhand 834001</div>
                 </div>
               </div>
             </div>
@@ -61,12 +61,8 @@ const Contact = () => {
                 <div className='col-8 col-md-8 col-lg-8 d-flex justify-content-start align-items-center'>
                   <div className='contact-font'>
                     <div>
-                      <a href="tel:+4733378901">+91 8457915348</a>
+                      <Link to="tel:+918789523890">+91 8789523890</Link>
                     </div>
-                    <div>
-                      <a href="tel:+4733378901">+91 8457915348</a>
-                    </div>
-
                   </div>
                 </div>
               </div>
@@ -83,7 +79,7 @@ const Contact = () => {
                 </div>
                 <div className='col-8 col-md-8 col-lg-8 d-flex justify-content-start  align-items-center'>
                   <div className='contact-font'>
-                    <a href="mailto:info@conterials.com">info@conterails.com</a>
+                    <Link to="mailto:info@conterials.com">info@conterails.com</Link>
                   </div>
                 </div>
               </div>
